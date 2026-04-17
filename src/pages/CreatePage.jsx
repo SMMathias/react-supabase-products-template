e807698084bcdac044a5509009df5e222d34e5ff
SMMathias <mathias08102004@gmail.com>
2026-04-17 10:51:54 +0200

    trin 3

--- src/pages/CreatePage.jsx
+++ src/pages/CreatePage.jsx
@@ -4,9 +4,19 @@ import ProductForm from "../components/ProductForm";
 export default function CreatePage() {
   const navigate = useNavigate();
 
+  const URL = import.meta.env.VITE_SUPABASE_URL;
+  const APIKEY = import.meta.env.VITE_SUPABASE_APIKEY;
   async function handleSubmit(productData) {
     console.log("CreatePage productData:", productData);
-    // TODO (Trin 3): Implementer POST med fetch, URL/APIKEY og body fra productData.
+    // TODO (Trin 3): Implementer POST med fetch, URL/APIKEY og body fra productData.'
+    await fetch(URL, {
+      method: "POST",
+      headers: {
+        apikey: APIKEY,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
     navigate("/");
   }
 
